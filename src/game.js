b9717b19ac04d6629f8d4f5427911d6ba9fae96a
Pablo Fernández Franco <pfranco@outlook.com>
2014-11-12 00:08:25 +0100

    Motor terminado y prbado

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 var engine = engine2d.create({
 	canvas: {
 		canvasElement: document.getElementById("canvas"),
-		width: 100,
-		height: 100
+		width: 300,
+		height: 300
 	}
 });
 
@@ -16,12 +16,17 @@ SceneTest.prototype.constructor = SceneTest;
 function EntityTest(content) {
 	engine2d.Entity.call(this, content.x, content.y);
 
-	this._sprite = Globals.engine.spriteFactory.getById(content.sprite);
+	this._sprite = Globals.engine.spriteFactory.getById(content.sprite, [this._x, this._y]);
 	//console.log(this._sprite);
 };
 
 EntityTest.prototype = Object.create(engine2d.Entity.prototype);
 EntityTest.prototype.constructor = EntityTest;
+
+EntityTest.prototype.update = function(dt) {
+	this._x += 50 * dt;
+	engine2d.Entity.prototype.update.call(this, dt);
+};
 
 
 var infoScenes = [
@@ -30,11 +35,10 @@ var infoScenes = [
 		obj: SceneTest,
 		content: {
 			entities: [
-				{name: "e3", x:0, y:76, o: EntityTest, sprite: "sp1"},
-				{name: "e4", x:58, y:4999, o: EntityTest, sprite: "sp1"}
+				{name: "e3", x:20, y:20, o: EntityTest, sprite: "sp1"}
 			],
 			sprites: [
-				{ id: "sp1", x:0, y:0, size: { w:10, h:10 }, resource: "http://www.herbalife.com.pa/Content/Global/img/layout/icon_arrow.gif" }
+				{ id: "sp1", size: { w:10, h:10 }, resource: "http://www.herbalife.com.pa/Content/Global/img/layout/icon_arrow.gif" }
 			]
 		}
 	},
