2b762c85a493fc5f75799ad321bcd592ec7f25a3
Prolo <fmrodriguezlopez@gmail.com>
2015-01-25 22:14:40 +0100

    Create character.js
    Constructor para el objeto "Character"

---
+++ src/character.js
@@ -0,0 +1,17 @@
+function Character(charname, coor) {
+
+	this.CharName = charname;
+	this.Coor[] = coor;
+
+	this.Move = funtion(){
+
+		//to do
+
+	};
+
+	this.Hunt = function(){
+
+		//to do
+
+	};
+}
