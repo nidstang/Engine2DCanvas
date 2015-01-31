5d60aaff60b82ba981dac7aba5357ae80cb0c791
Prolo <fmrodriguezlopez@gmail.com>
2015-01-31 20:13:46 +0100

    Update character.js
    Completada funcion de moverse

--- src/character.js
+++ src/character.js
@@ -3,9 +3,54 @@ function Character(charname, coor) {
 	this.CharName = charname;
 	this.Coor[] = coor;
 
-	this.Move = funtion(){
+	this.Move = funtion(cardinal){
 
-		//to do
+		if (cardinal == "N")
+		{
+			if this.Coor[1] == 0
+			{
+				console.log("You have reached the limit of the map");
+			}
+			else
+			{
+				this.Coor[1] = this.Coor[1] - 1;	
+			}
+			
+		}
+		else if (cardinal == "S")
+		{
+			if this.Coor[1] == 100 //FIN DEL MAPA ¿LLAMADA AL MAPCONTAINER?
+			{
+				console.log("You have reached the limit of the map")
+			}
+			else
+			{
+				this.Coor[1] = this.Coor[1] + 1;
+			}
+		}
+		else if (cardinal == "E")
+		{
+			if this.Coor[0] == 100 //FIN DEL MAPA ¿LLAMADA AL MAPCONTAINER?
+			{
+				console.log("You have reached the limit of the map")
+			}
+			else
+			{
+				this.Coor[0] = this.Coor[0] + 1;
+			}
+			
+		}
+		else if (cardinal == "W")
+		{
+			if this.Coor[0] == 0
+			{
+				console.log("You have reached the limit of the map");
+			}
+			else
+			{
+				this.Coor[0] = this.Coor[0] - 1;	
+			}
+		}
 
 	};
 
