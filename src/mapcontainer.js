486435fcf6e3c9fe8e921d920a018720fda1a322
Prolo <fmrodriguezlopez@gmail.com>
2015-01-31 21:23:41 +0100

    Update mapcontainer.js

--- src/mapcontainer.js
+++ src/mapcontainer.js
@@ -1,1 +1,31 @@
 //contenedor del worldmap
+function MapContainer()
+{
+  this.MaxX = 50;
+  this.MaxY = 50;
+
+  this.MatrixMap[];
+  
+  this.Initialize = function(this.MaxX, this.MaxY, this.MatrixMap) //Inicializa una matriz de objetos tipo Mapa
+  {
+    for(i = 0; i < MaxX; i++)
+    {
+      MatrixMap[i] = new Array();
+      
+      for(j = 0; i < MaxY; j++)
+      {
+        MatrixMap[i][j] = New Map(new Array(i,j), new Array());
+      }
+    }
+  }
+  
+  this.GetMaxX = function()
+  {
+    return this.MaxX;
+  }
+  
+  this.GetMaxY = function()
+  {
+    return this.MaxY;
+  }
+}
